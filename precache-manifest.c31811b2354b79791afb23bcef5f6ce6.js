cc7174c17a2744ada43defaf6766b4f0b1738636
jelel6 <abduljelelahmed@gmail.com>
2021-02-12 02:34:29 -0800

    Updates

---
+++ precache-manifest.c31811b2354b79791afb23bcef5f6ce6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robo_pwa/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "f1e90fa2d0eb1d758d89",
+    "url": "/robo_pwa/static/js/runtime~main.abf3cbe2.js"
+  },
+  {
+    "revision": "3051b493a81d6d6b54e2",
+    "url": "/robo_pwa/static/js/main.1aa4c48e.chunk.js"
+  },
+  {
+    "revision": "709d949f2929ed0708c6",
+    "url": "/robo_pwa/static/js/2.142f09da.chunk.js"
+  },
+  {
+    "revision": "3051b493a81d6d6b54e2",
+    "url": "/robo_pwa/static/css/main.34ae3a02.chunk.css"
+  },
+  {
+    "revision": "709d949f2929ed0708c6",
+    "url": "/robo_pwa/static/css/2.b1213e29.chunk.css"
+  },
+  {
+    "revision": "b75fb83d9e22caf0beaf8871a36dbdbe",
+    "url": "/robo_pwa/index.html"
+  }
+];
