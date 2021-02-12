ca0a38beb43f4c8ca85ed5d4b8977eb762e41f97
jelel6 <abduljelelahmed@gmail.com>
2021-02-12 04:01:57 -0800

    Updates

---
+++ precache-manifest.84380ffbae91056162b3e37d3a3707de.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robo_pwaa/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "5bdedf4dd59391c286c7",
+    "url": "/robo_pwaa/static/js/runtime~main.31576165.js"
+  },
+  {
+    "revision": "648d57e9310e1358b506",
+    "url": "/robo_pwaa/static/js/main.02690c5e.chunk.js"
+  },
+  {
+    "revision": "41feebb74b4b8edaf246",
+    "url": "/robo_pwaa/static/js/2.f62e9e97.chunk.js"
+  },
+  {
+    "revision": "648d57e9310e1358b506",
+    "url": "/robo_pwaa/static/css/main.c6b76a58.chunk.css"
+  },
+  {
+    "revision": "41feebb74b4b8edaf246",
+    "url": "/robo_pwaa/static/css/2.b1213e29.chunk.css"
+  },
+  {
+    "revision": "654e1e9be88321efe55cf86134fcb93c",
+    "url": "/robo_pwaa/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/robo_pwa/precache-manifest.aa1babd401f70507378444cc6126cd3f.js"
+  "/robo_pwaa/precache-manifest.84380ffbae91056162b3e37d3a3707de.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/robo_pwa/index.html", {
+workbox.routing.registerNavigationRoute("/robo_pwaa/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
